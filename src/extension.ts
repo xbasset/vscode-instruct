dfdc2cfbf7531c74306d9cf07218dab5c89fe628
xbasset <xavier.basset@hoomano.com>
2024-06-09 18:51:46 +0200

    working tag color

--- src/extension.ts
+++ src/extension.ts
@@ -8,18 +8,9 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-instruct" is now active!');
+	console.log('vscode-instruct is active');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-instruct.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('This is the new `instruct` extension!');
-	});
 
-	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
